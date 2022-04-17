4cfd837ac7023d3ae2f2ae40fc9ffafc9e1326cd
Brisolo32 <94653435+Brisolo32@users.noreply.github.com>
2022-04-17 09:58:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+process.env.NTBA_FIX_319 = 1;
+
 const tmi = require('tmi.js');
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+const prompt = require('prompt-sync')({sigint: true});
+var channel = prompt('Channel to listen for messages: ');
 
 const { twitchbotname, oauthtoken, channelname, teletoken, chatid } = require('./config.json');
 
@@ -19,7 +24,10 @@ const client = new tmi.Client({
 
 client.connect();
 
+client.once('connected', () => {
+	bot.sendMessage(chatId, `<b>Connected to ${channel}</b>`, { parse_mode: 'HTML' });
+});
+
 client.on('message', (channel, tags, message, self) => {
     bot.sendMessage(chatid, `<b>${tags['display-name']}:</b> ${message}`,{parse_mode : "HTML"});
-});
-		
+});	
